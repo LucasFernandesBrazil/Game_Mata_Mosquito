517a319692151de6fa1d2ef1197dca17b5b83631
Lucas Fernandes <lucasfernandesprogrammer@gmail.com>
2021-08-13 18:33:50 -0300

    Configuração de níveis

--- jogo.js
+++ jogo.js
@@ -3,6 +3,21 @@ var largura = 0
 var vidas = 1
 var tempo = 5
 
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if (nivel === 'normal') {
+    criaMosquitoTempo = 1500
+} 
+else if (nivel === 'dificil') {
+    criaMosquitoTempo = 1000
+}
+else if (nivel === 'chuckNorris') {
+    criaMosquitoTempo = 750
+}
+
 function ajustaTamanhoPalcoJogo () {
     altura = window.innerHeight
     largura = window.innerWidth
